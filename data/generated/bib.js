250f083e7d5af52f750bb016862e4f0cbc5375ce
Wjc0323 <119430544+Wjc0323@users.noreply.github.com>
2023-05-14 18:08:30 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,103 @@
+﻿const generatedBibEntries = {
+    "BOUSSAID201382": {
+        "abstract": "Metaheuristics are widely recognized as efficient approaches for many hard optimization problems. This paper provides a survey of some of the main metaheuristics. It outlines the components and concepts that are used in various metaheuristics in order to analyze their similarities and differences. The classification adopted in this paper differentiates between single solution based metaheuristics and population based metaheuristics. The literature survey is accompanied by the presentation of references for further details, including applications. Recent trends are also briefly discussed.",
+        "author": "Ilhem Boussa\u00efd and Julien Lepagnot and Patrick Siarry",
+        "doi": "https://doi.org/10.1016/j.ins.2013.02.041",
+        "issn": "0020-0255",
+        "journal": "Information Sciences",
+        "keywords": "Population based metaheuristic, Single solution based metaheuristic, Intensification, Diversification",
+        "note": "Prediction, Control and Diagnosis using Advanced Neural Computations",
+        "pages": "82-117",
+        "title": "A survey on optimization metaheuristics",
+        "type": "article",
+        "url": "https://www.sciencedirect.com/science/article/pii/S0020025513001588",
+        "volume": "237",
+        "year": "2013"
+    },
+    "burke2011hyflex": {
+        "author": "Burke, Edmund and Curtois, Tim and Hyde, Matthew and Ochoa, Gabriela and Vazquez-Rodriguez, Jose A",
+        "journal": "arXiv preprint arXiv:1107.5462",
+        "title": "Hyflex: A benchmark framework for cross-domain heuristic search",
+        "type": "article",
+        "year": "2011"
+    },
+    "burke2013hyper": {
+        "author": "Burke, Edmund K and Gendreau, Michel and Hyde, Matthew and Kendall, Graham and Ochoa, Gabriela and {\\\"O}zcan, Ender and Qu, Rong",
+        "journal": "Journal of the Operational Research Society",
+        "pages": "1695--1724",
+        "publisher": "Springer",
+        "title": "Hyper-heuristics: A survey of the state of the art",
+        "type": "article",
+        "volume": "64",
+        "year": "2013"
+    },
+    "el2010vehicle": {
+        "author": "El-Sherbeny, Nasser A",
+        "journal": "Journal of King Saud University-Science",
+        "number": "3",
+        "pages": "123--131",
+        "publisher": "Elsevier",
+        "title": "Vehicle routing with time windows: An overview of exact, heuristic and metaheuristic methods",
+        "type": "article",
+        "volume": "22",
+        "year": "2010"
+    },
+    "elshaer2020taxonomic": {
+        "author": "Elshaer, Raafat and Awad, Hadeer",
+        "journal": "Computers \\& Industrial Engineering",
+        "pages": "106242",
+        "publisher": "Elsevier",
+        "title": "A taxonomic review of metaheuristic algorithms for solving the vehicle routing problem and its variants",
+        "type": "article",
+        "volume": "140",
+        "year": "2020"
+    },
+    "hussain2019metaheuristic": {
+        "author": "Hussain, Kashif and Mohd Salleh, Mohd Najib and Cheng, Shi and Shi, Yuhui",
+        "journal": "Artificial intelligence review",
+        "pages": "2191--2233",
+        "publisher": "Springer",
+        "title": "Metaheuristic research: a comprehensive survey",
+        "type": "article",
+        "volume": "52",
+        "year": "2019"
+    },
+    "liu2023heuristics": {
+        "author": "Liu, Fei and Lu, Chengyu and Gui, Lin and Zhang, Qingfu and Tong, Xialiang and Yuan, Mingxuan",
+        "journal": "arXiv preprint arXiv:2303.04147",
+        "title": "Heuristics for Vehicle Routing Problem: A Survey and Recent Advances",
+        "type": "article",
+        "year": "2023"
+    },
+    "qu2020general": {
+        "author": "Qu, Rong and Kendall, Graham and Pillay, Nelishia",
+        "journal": "IEEE Computational Intelligence Magazine",
+        "number": "2",
+        "pages": "14--23",
+        "publisher": "IEEE",
+        "title": "The general combinatorial optimization problem: Towards automated algorithm design",
+        "type": "article",
+        "volume": "15",
+        "year": "2020"
+    },
+    "vidal2013heuristics": {
+        "author": "Vidal, Thibaut and Crainic, Teodor Gabriel and Gendreau, Michel and Prins, Christian",
+        "journal": "European Journal of Operational Research",
+        "number": "1",
+        "pages": "1--21",
+        "publisher": "Elsevier",
+        "title": "Heuristics for multi-attribute vehicle routing problems: A survey and synthesis",
+        "type": "article",
+        "volume": "231",
+        "year": "2013"
+    },
+    "walker2012vehicle": {
+        "author": "Walker, James D and Ochoa, Gabriela and Gendreau, Michel and Burke, Edmund K",
+        "booktitle": "Learning and Intelligent Optimization: 6th International Conference, LION 6, Paris, France, January 16-20, 2012, Revised Selected Papers",
+        "organization": "Springer",
+        "pages": "265--276",
+        "title": "Vehicle routing and adaptive iterated local search within the hyflex hyper-heuristic framework",
+        "type": "inproceedings",
+        "year": "2012"
+    }
+};
